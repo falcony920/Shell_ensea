7808c2b0791d96304485a6df6f418784453a7140
Falcony <faucon602@gmail.com>
2024-11-27 14:23:40 +0100

    Initial commit

---
+++ enseash.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main()
+{
+    // Display welcome message
+    write(STDOUT_FILENO, "Welcome to ENSEA Tiny Shell.\nType 'exit' to quit.\nenseash % ", 60);
+
+    char command[128];
+    while (1)
+    {
+        // Read user input
+        ssize_t n = read(STDIN_FILENO, command, sizeof(command) - 1);
+        if (n <= 0)
+            break;
+
+        // Remove newline character
+        command[n - 1] = '\0';
+
+        // Exit condition
+        if (strcmp(command, "exit") == 0)
+        {
+            write(STDOUT_FILENO, "Bye bye...\n", 11);
+            break;
+        }
+
+        // Display prompt again
+        write(STDOUT_FILENO, "enseash % ", 10);
+    }
+    return 0;
+}
